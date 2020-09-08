4f09a5a85b3117311a9d7b8c95a6a419076ad40e
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-09-08 11:23:49 +0100

    add

--- cmd/paniccleanup/paniccleanup.go
+++ cmd/paniccleanup/paniccleanup.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
+	var file io.ReadCloser
+	file, err := OpenCSV("cmd/paniccleanup/data.csv")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer file.Close()
 
+	// Do something with file
 }
 
 func OpenCSV(filename string) (file *os.File, err error) {
-	defer func(){
+	defer func() {
 		if r := recover(); r != nil {
 			file.Close()
 			err = r.(error)
 		}
+	}()
+	file, err = os.Open(filename)
+	if err != nil {
+		fmt.Printf("failed to open file\n")
+		return file, err
 	}
+
+	RemoveEmptyLines(file)
+
+	return file, err
 }
 
 func RemoveEmptyLines(f *os.File) {
